841536bfb306f310cfbd9c97e498c9aac976e0e4
EmilyDSarani <emily.sarani@gmail.com>
2022-01-05 20:03:38 -0600

    i think I got it

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -5,8 +5,30 @@ const UserContext = createContext();
 
 //Create Provider
 const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(
+    const [user, setUser] = useState('')
 
-//userAuth
-//useUser Custom Hook
+    //userAuth
+    const login = (email, password) =>{
+        const loginSuccessful =
+        email === '' &&
+        password === '';
+        if(loginSuccessful) setUser(email);
+        return loginSuccessful
+    }
+    //set logout
+    const logout =(callback) =>{
+        setUser(null);
+        callback();
+    };
+    return <UserContext.Provider value={{user, setUser, login, logout}}>{children}</UserContext.Provider>
 }
+    //useUser Custom Hook
+    const useUser = () => {
+        const context = useContext(UserContext)
+
+        if(context === undefined){
+            throw new Error('useUser needs to be defined in UserContext Provider')
+        }
+    }
+
+
