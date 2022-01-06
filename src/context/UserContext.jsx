74451cc87118962f558a49ba6d76930423cbb75b
EmilyDSarani <emily.sarani@gmail.com>
2022-01-05 19:48:09 -0600

    I dont think I fully know how to utilize UserContext in reference to this vs old code vs when we put the auth in it?

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,12 @@
+import { useContext, createContext, useState } from 'react'
+
+//Create Context
+const UserContext = createContext();
+
+//Create Provider
+const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(
+
+//userAuth
+//useUser Custom Hook
+}
